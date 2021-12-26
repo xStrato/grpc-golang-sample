a38b680f5ec8963bfe245488c72a3110fe4dc461
xStrato <metalofgear@hotmail.com>
2021-12-26 12:41:08 -0300

    update

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net"
 
+	"github.com/xStrato/grpc-golang-sample/pb"
+	"github.com/xStrato/grpc-golang-sample/services"
 	"google.golang.org/grpc"
 )
 
@@ -13,9 +15,10 @@ func main() {
 		log.Fatalf("Could not listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
 
 	if err := grpcServer.Serve(list); err != nil {
-		log.Fatalf("Could not serve %v", err)
+		log.Fatalf("Could not serve: %v", err)
 	}
 	log.Println("server is listening on port: 3030")
 }
